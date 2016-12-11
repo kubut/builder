a0d73c1e0c6e3b74ba75f0ad01858e2e3f3ed3ac
kubut <fotokubut@gmail.com>
2016-12-11 15:28:45 +0100

    Added jira information on dashboard

--- source_js/source/ts/dashboard/IInstance.ts
+++ source_js/source/ts/dashboard/IInstance.ts
@@ -3,9 +3,17 @@ module APP.Dashboard {
         id: number;
         name: string;
         status: InstanceStatus;
+        jiraInformation?: IJiraInformation;
         branchName: string;
         buildDate: string;
         author: string;
+        url: string;
+    }
+
+    export interface IJiraInformation {
+        title: string;
+        description: string;
+        reporter: string;
         url: string;
     }
 }
